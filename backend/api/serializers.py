dbdcf6d244ffd9818fb8c9a8d80d59ac6944f223
AndreGomesN <gomes10andre@gmail.com>
2024-08-17 16:37:15 -0300

    feat: create user and access/refresh token

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -6,3 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id','username','password']
         extra_kwargs = {"password": {"write_only":True}}
+
+    def create(self,validate_data):
+        user = User.objects.create_user(**validate_data)
+        return user

dd5bcc0027dfbae1bb51c5082c23217ddc486c4c
AndreGomesN <gomes10andre@gmail.com>
2024-08-17 23:04:25 -0300

    feat: note table and create/delete note if user autheticated

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('api/token/',TokenObtainPairView.as_view(),name='get-token'),
     path('api/token/refresh/',TokenRefreshView.as_view(),name='refresh-token'),
     path('api-auth/',include("rest_framework.urls")),
+    path('api/',include("api.urls")),
 ]
